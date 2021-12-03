1dc43e03e339cd823cd2b7f94cd277f31a7bb5bc
Maikon Muniz <maikon.muni@yahoo.com.br>
2021-12-03 01:17:17 -0300

    implements post using sequelize

--- server.js
+++ server.js
@@ -2,26 +2,45 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 const conn = require('./db/conn')
+const User = require('./models/User')
 
-const hbs = exphbs.create({
-    partialsDir: ['views/partials'],
-})
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
 
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars');  
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars');
 
 
 app.get('/', (req, res) => {
+    res.render('home')
+})
 
-    const User = {
-        'name': 'maikon'
+app.get('/users/create', (req, res) => {
+    res.render('adduser')
+})
+
+app.post('/users/create', async (req, res) => {
+    console.log(req.body)
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if(newsletter === 'on'){
+        newsletter = true
+    }else{
+        newsletter = false
     }
 
-    const auth = true
+    await User.create({name, occupation, newsletter})
 
-    res.render('home', {User, auth});
-});
+    res.redirect('/')
+})
 
-app.listen(3000, () => {
-    console.log('App funcionando');
-});
+conn.sync().then(() => {
+        app.listen(3000)
+})
+  .catch((err) => console.log(err))
